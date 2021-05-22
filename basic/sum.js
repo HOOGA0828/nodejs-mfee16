c9849b5c8d098feaf141f0b53d6a718415f15114
HOOGA <popo51102@gmail.com>
2021-05-22 16:11:46 +0800

    sum.js

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+let result=0;
+function sum(n){
+    for(let i=1; i<=n; i++){
+        result+=i;
+    }
+    return result;
+}
+
+console.log(sum(1));
+console.log(sum(5));
+console.log(sum(10));
+console.log(sum(100));
+
+
