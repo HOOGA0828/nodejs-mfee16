8491a6d18afc3326cebe5be701ef0d8bae9cab7e
HOOGA <popo51102@gmail.com>
2021-06-01 17:12:42 +0800

    update moment and promise

--- crawler/app.js
+++ crawler/app.js
@@ -1,22 +1,56 @@
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
 
-var axios = require('axios');
+function readPromise() {
+  return new Promise(function (resolve, reject) {
+    fs.readFile("stock.txt", "utf-8", function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
-    params: {
-        response:'json',
-        date:'20210523',
-        stockNo:'2610',
-    }
+readPromise()
+  .then((data) => {
+    console.log(`股票番號: ${data}`);
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: data,
+      },
+    });
   })
   .then(function (response) {
-    let result = response.data;
-    console.log(result.date);
-    console.log(result.title);
+    if (response.data.stat === "OK") {
+      console.log("選擇日期:", response.data.date);
+      console.log(response.data.title);
+    }
   })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });  
+  .catch((err) => {
+    console.error(err);
+  });
 
+// axios
+//   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?", {
+//     params: {
+//       response: "json",
+//       date: "20210523",
+//       stockNo: "2610",
+//     },
+//   })
+//   .then(function (response) {
+//     let result = response.data;
+//     console.log(result.date);
+//     console.log(result.title);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
