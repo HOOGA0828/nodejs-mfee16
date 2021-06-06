270ca58304a48e56f328836d201208037a211db4
HOOGA <popo51102@gmail.com>
2021-06-06 11:00:53 +0800

    update .env

--- crawler/app-sql.js
+++ crawler/app-sql.js
@@ -6,10 +6,10 @@ const mysql = require("mysql");
 
 // 建立連線
 let connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "stock"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 connection = Promise.promisifyAll(connection);
