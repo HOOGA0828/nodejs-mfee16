7afefaec88f60103c60cff7716744c3b4261be05
HOOGA <popo51102@gmail.com>
2021-06-06 11:09:47 +0800

    update .ev

--- crawler/app-sql.js
+++ crawler/app-sql.js
@@ -2,14 +2,15 @@ const Promise = require("bluebird");
 const fs = require("fs/promises");
 const axios = require("axios");
 const mysql = require("mysql");
+require("dotenv").config();
 
 
 // 建立連線
 let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME
 });
 
 connection = Promise.promisifyAll(connection);
